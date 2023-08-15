c8301b25cc65ebca4aeb6f00536d83c2da14b483
xb <song.xiaobing@h3c.com>
2023-08-15 15:35:53 +0800

    fio

--- start.sh
+++ start.sh
@@ -2,7 +2,10 @@
 # ../src/vstart.sh --debug --new
 # cd build
 FS=0 OSD=3 MON=3 MGR=3 ../src/vstart.sh --debug --new
-echo -e "create_pool: ceph osd pool create p1 3"
+echo -e "export CEPH_CONF=/home/xb/project/ceph/xb/ceph/build/ceph.conf"
+echo -e "export PATH=/home/xb/project/ceph/xb/ceph/build/bin:$PATH"
+
+echo -e "\n\n\ncreate_pool: ceph osd pool create p1 3"
 
 # export PYTHONPATH=/home/xb/project/stor/ceph/xb/docker/ceph/src/pybind:/home/xb/project/stor/ceph/xb/docker/ceph/build/lib/cython_modules/lib.3:/home/xb/project/stor/ceph/xb/docker/ceph/src/python-common:$PYTHONPATH
 # export LD_LIBRARY_PATH=/home/xb/project/stor/ceph/xb/docker/ceph/build/lib:$LD_LIBRARY_PATH
